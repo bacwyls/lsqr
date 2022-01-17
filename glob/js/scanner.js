19d486a3ad97dd67defcf6857f4c4705d7dfaa1a
bacwyls <bacwyls@gmail.com>
2022-01-16 19:34:35 -0700

    actually fix script src...

--- glob/js/scanner.js
+++ glob/js/scanner.js
@@ -1,7 +1,6 @@
 $(document).ready ( function(){
   var videoElem = document.getElementById("show-camera");
-  // const qrScanner = new QrScanner(videoElem, result => handleScanResult(result));
-  const qrScanner = new QrScanner(videoElem, result => console.log('decoded qr code:', result));
+  const qrScanner = new QrScanner(videoElem, result => handleScanResult(result));
   qrScanner.start();
 
   function handleScanResult(result) {
