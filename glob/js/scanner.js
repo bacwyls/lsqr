ac24a6df5ad9339449a7160cb75a834b4e81b582
bacwyls <bacwyls@gmail.com>
2022-01-20 09:49:08 -0700

    works on ios

--- glob/js/scanner.js
+++ glob/js/scanner.js
@@ -1,13 +1,25 @@
 $(document).ready ( function(){
   var videoElem = document.getElementById("show-camera");
+
+
+  QrScanner.WORKER_PATH = "js/qr-scanner/qr-scanner-worker.min.js";
   const qrScanner = new QrScanner(videoElem, result => handleScanResult(result));
   qrScanner.start();
 
+  var wFoundElem = document.getElementById("found-patp-w");
+  var foundElem = document.getElementById("found-patp");
   function handleScanResult(result) {
     if(result[0] != "~") {
       return;
     }
-    window.location.href = "/apps/landscape/~profile/"+result;
+    console.log("DETECTED: "+result);
+    qrScanner.stop();
+
+    wFoundElem.style.display = "";
+    videoElem.style.display = "none";
+
+    foundElem.innerHTML = result;
+    foundElem.href = "/apps/landscape/~profile/"+result;
   }
 });
 
